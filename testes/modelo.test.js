cfa2aea89d34ed1d751ef9b6432ca92569c6bb15
leosalim <leosalim2@live.com>
2023-10-28 22:59:57 -0300

    Extensão de cobertura de testes

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,23 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando cadastro de três respostas', () => {
+  const id_pergunta = modelo.cadastrar_pergunta('1 + 1 = ?');
+  modelo.cadastrar_resposta(id_pergunta, '1 + 1 = 2');
+  modelo.cadastrar_resposta(id_pergunta, '1 + 1 = 3');
+  modelo.cadastrar_resposta(id_pergunta, '1 + 1 = 4');
+  const respostas = modelo.get_respostas(id_pergunta);
+  expect(respostas.length).toBe(3);
+  expect(respostas[0].texto).toBe('1 + 1 = 2');
+  expect(respostas[1].texto).toBe('1 + 1 = 3');
+  expect(respostas[2].texto).toBe('1 + 1 = 4');
+  expect(respostas[1].id_resposta).toBe(respostas[2].id_resposta-1);
+});
+
+test('Testando obter pergunta por id', () => {
+  const id_pergunta = modelo.cadastrar_pergunta('1 + 1 = ?');
+  const pergunta = modelo.get_pergunta(id_pergunta);
+  expect(pergunta.id_pergunta).toBe(id_pergunta);
+  expect(pergunta.texto).toBe('1 + 1 = ?');
+});
